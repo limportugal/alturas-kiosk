1a12fdbd306c247317bf0fe07ab8f4d39ad57fd8
limportugal <limuelportugal@gmail.com>
2026-06-08 17:24:43 +0800

    migrate

--- database/migrations/2026_06_08_060209_add_variation_quantity_table.php
+++ database/migrations/2026_06_08_060209_add_variation_quantity_table.php
@@ -26,6 +26,6 @@ return new class extends Migration
     {
         Schema::table('product_color_variants', function (Blueprint $table) {
             $table->dropColumn('quantity');
-        });
+        }); 
     }
 };
